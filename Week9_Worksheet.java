a436a029e4ef2e281e3f692ef107d93f9f13dd2f
Daniel <2530910@brunel.ac.uk>
2025-11-26 10:47:31 +0000

    added new methods for the seminar activity

--- Week9_Worksheet.java
+++ Week9_Worksheet.java
@@ -7,6 +7,12 @@ public class Week9_Worksheet {
 	public static void main(String[] args) {
 		int[] temp = {13, 44, 16, 11};
 		System.out.println(Arrays.toString(celsiusToFahrenheit(temp)));
+		
+		int[] tempWithNeg = {-13, -44, -16, 45, 66};
+		System.out.println(Arrays.toString(celsiusToFahrenheitWithoutNegative(tempWithNeg)));
+		
+		String[] tempInString = {"13", "3", "34", "26"};
+		System.out.println(Arrays.toString(celsiusToFahrenheitInString(tempInString)));
 
 	}
 	
@@ -15,7 +21,38 @@ public class Week9_Worksheet {
 		double[] fahrenheit = new double[celsius.length];
 		
 		for(int i=0; i < fahrenheit.length; i++) {
-			fahrenheit[i] = celsius[i] * 9/5 + 32;
+			fahrenheit[i] = celsius[i] * 9.0/5 + 32;
+		}
+		
+		return fahrenheit;
+	}
+	
+	public static double[] celsiusToFahrenheitWithoutNegative(int[] celsius) {
+		double[] fahrenheit = new double[celsius.length];
+		
+		for(int i=0; i < fahrenheit.length; i++) {
+			if (celsius[i] <= 0) {
+				fahrenheit[i] = 0.0;
+				
+			}
+			else{
+				fahrenheit[i] = celsius[i] * 9.0/5 + 32;
+			}
+			
+		}
+		
+		return fahrenheit;
+	}
+	
+	public static double[] celsiusToFahrenheitInString(String[] celsius) {
+		double[] fahrenheit = new double[celsius.length];
+		int[] celsiusInInt = new int[celsius.length];
+		for(int i=0; i < celsius.length; i++) {
+			celsiusInInt[i] = Integer.parseInt(celsius[i]);
+		}
+		
+		for(int i=0; i < fahrenheit.length; i++) {
+			fahrenheit[i] = celsiusInInt[i] * 9.0/5 + 32;
 		}
 		
 		return fahrenheit;
